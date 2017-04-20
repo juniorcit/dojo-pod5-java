13feda82b8ea6d637a7be286df1052b15cbb14a0
Marcos Santini <msantini@ciandt.com>
2017-04-20 14:21:48 -0300

    adicionado leitura do buffer para o csvparser

--- src/test/java/com/ciandt/dojo/utils/CsvParserTest.java
+++ src/test/java/com/ciandt/dojo/utils/CsvParserTest.java
@@ -2,21 +2,33 @@ package com.ciandt.dojo.utils;
 
 import org.junit.Test;
 
-import javax.validation.constraints.AssertTrue;
-
+import java.io.IOException;
 import java.io.Reader;
-
-import static org.junit.Assert.*;
+import java.io.UncheckedIOException;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 
 public class CsvParserTest {
 
-    private CsvParser parser = new CsvParser();
+    private CsvParser parser = createCsvParser();
 
     @Test
-    public void testIfCsvIsLoaded()
-    {
-        
+    public void testIfCsvIsLoaded() {
+
+    }
+
+    private CsvParser createCsvParser() {
+        final Path path = Paths.get("src/main/resources", "exams.csv");
+
+        try {
+            Reader reader = Files.newBufferedReader(path, Charset.forName("UTF-8"));
+            return new CsvParser(reader);
+        } catch (IOException ex) {
+            throw new UncheckedIOException(ex);
+        }
     }
 
 
