bb832a116045b0d41fdd253c614f0dabb9c8f2ea
Marcos Santini <msantini@ciandt.com>
2017-04-20 14:08:39 -0300

    csv class initializer

---
+++ src/main/java/com/ciandt/dojo/utils/CsvParser.java
@@ -0,0 +1,12 @@
+package com.ciandt.dojo.utils;
+
+import java.io.Reader;
+
+public class CsvParser {
+    private static String PATH = "src/main/resources/exams.csv";
+
+
+    public CsvParser(final Reader readerFromBufer) {
+
+    }
+}
---
+++ src/test/java/com/ciandt/dojo/utils/CsvParserTest.java
@@ -0,0 +1,23 @@
+package com.ciandt.dojo.utils;
+
+import org.junit.Test;
+
+import javax.validation.constraints.AssertTrue;
+
+import java.io.Reader;
+
+import static org.junit.Assert.*;
+
+
+public class CsvParserTest {
+
+    private CsvParser parser = new CsvParser();
+
+    @Test
+    public void testIfCsvIsLoaded()
+    {
+        
+    }
+
+
+}
